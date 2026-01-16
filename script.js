ff5eb1f531bb601e69040330ef068cbb2cf82f23
Aaditya Kumar Mandal <aaditya5437a@gmail.com>
2026-01-16 20:37:38 +0545

    Initial website upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("show");
+  }
+  
+  function toggleTheme() {
+    document.body.classList.toggle("light");
+  }
+  
